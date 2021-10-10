b5381ec1f0935323f34ceb5409f7517a051ce601
aydinmerve <merve9328@gmail.com>
2021-10-10 16:19:34 +0300

    progress bar dinamics

--- app.js
+++ app.js
@@ -15,29 +15,19 @@ new Vue({
             //this.monster_heal=this.monster_heal-point;
             this.monster_heal -= point;
             this.monster_attack();
-            console.log('M :'  + this.monster_heal);
-            console.log('P :' + this.player_heal);
-
         },
         special_attack: function(){
             var point = Math.ceil(Math.random() * 25);
             this.monster_heal -= point;
             this.monster_attack();
-            console.log('M :'  + this.monster_heal);
-            console.log('P :' + this.player_heal);
-
         },
         heal_up: function(){
             var point = Math.ceil(Math.random() * 20);
             this.player_heal += point;
-            this.monster_attack();
-            console.log('M :'  + this.monster_heal);
-            console.log('P :' + this.player_heal);
+            this.monster_attack();      
         },
         give_up: function(){
            this.player_heal= 0;
-           console.log('M :'  + this.monster_heal);
-           console.log('P :' + this.player_heal);
         },
         monster_attack: function(){
             var point = Math.ceil(Math.random() * 15);
